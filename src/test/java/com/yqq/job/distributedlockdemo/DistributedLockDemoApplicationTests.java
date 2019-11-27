4e03d2132d2168e2e0910d0c31a79e926a43efa2
yqq14978 <18382258214@163.com>
2019-11-27 11:29:21 +0800

    创建项目

---
+++ src/test/java/com/yqq/job/distributedlockdemo/DistributedLockDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yqq.job.distributedlockdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedLockDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
